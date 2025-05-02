5d72806f466351bf32434cdaf743bfdbe2472649
felipestawinski <felipe.stawinski@gmail.com>
2025-05-01 22:31:04 -0300

    changes

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ def mask(image, threshold):
     rows, cols, channels = image.shape
     for row in range(rows):
         for col in range(cols):
-            if image[row, col] < threshold:
-                image[row, col] = 0
+            if np.all(image[row, col] < threshold):
+                image[row, col] = 0.0
 
     cv2.imshow('Edited Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     return
 
 def main():
@@ -16,12 +18,14 @@ def main():
     if image is None:
         print("Error: Could not read the image.")
         return
+    #image = image.reshape ((image.shape [0], image.shape [1], 1))
+    image = image.astype (np.float32) / 255
     
     # cv2.imshow('Original Image', image)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
     
-    #mask(image, )
+    mask(image, 0.8)
 
 
 if __name__ == "__main__":
